63ae049c3ba8c6e35683bda352293df59a40a6c6
Krista.Oke <krista.oke@nmfs.local>
2023-03-08 16:51:19 -0900

    slowly adding datasets

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -6,6 +6,25 @@
 #Notes:
 #====================================================================
 
+require(tidyverse)
+require(corrplot)
+
+
+#=============================================================
+#### Define Directory Structure ####
+wd <- getwd()
+
+dir.data <- file.path(wd,"data")
+dir.output <- file.path(wd,"output")
+dir.figs <- file.path(wd,"figs")
+
+#Get data plot data =======
+
+birddat <- read.csv(file.path(dir.data, "MDO_age0sablefish_indices.csv"))
+
+birdtemp <-  read.csv(file.path(dir.data, "MDO_foragingarea_meanSST_year_month.csv"))
+
+adfgdat <- read.csv(file.path(dir.data, "sablefishlengths88to22.csv"))
 
 
 
