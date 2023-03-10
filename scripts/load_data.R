cc160046dc9d7f5fe2f00786600b56934c871c20
Krista.Oke <krista.oke@nmfs.local>
2023-03-10 14:17:51 -0900

    loading processing AKesp package data

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -10,8 +10,10 @@
 #Metadata, citation info in xml file in data folder
 #====================================================================
 
-require(tidyverse)
-require(corrplot)
+library(tidyverse)
+library(corrplot)
+library(AKesp)
+devtools::install_local("C:/Users/Krista.Oke/Analyses/Getting started/AKesp-main.zip")
 
 
 #=============================================================
@@ -99,6 +101,93 @@ zbirdtempwide <-zbirdtemp %>% pivot_wider(names_from = month, values_from = temp
 
 #oof adfg dat needs processing
 
+
+
+#import from AKesp package------
+
+AKesp::esp_stock_options() #look at stocks and pick one
+sbltbl <- AKesp::get_esp_data(stock="Alaska Sablefish")
+
+AKesp::check_data(sbltbl)
+
+#drop socioecon indicators
+sbltbl <- sbltbl[which(sbltbl$INDICATOR_TYPE=="Ecosystem"),]
+
+sblwide <- pivot_wider(sbltbl[,c(1:3)], names_from = INDICATOR_NAME, values_from = "DATA_VALUE")
+ 
+colnames(sblwide)
+
+sblbigsub <- sblwide[,c("YEAR",
+                    "Annual_Heatwave_GOA_Model",
+                    "Annual_Sablefish_Growth_YOY_Middleton_Survey",
+                    "Spring_Temperature_Surface_EGOA_Satellite",                      
+                    "Spring_Temperature_Surface_GOA_Satellite",                       
+                    "Spring_Temperature_Surface_SEBS_Satellite",
+                    "Summer_Sablefish_CPUE_Juvenile_GOA_Survey",                      
+                    "Summer_Sablefish_CPUE_Juvenile_Nearshore_GOAAI_Survey",
+                    "Summer_Temperature_250m_GOA_Survey",
+                     "Spring_Chlorophylla_Biomass_EGOA_Satellite" ,                    
+                     "Spring_Chlorophylla_Biomass_GOA_Satellite"  ,                    
+                     "Spring_Chlorophylla_Biomass_SEBS_Satellite",
+                    #"Summer_Euphausiid_Abundance_Kodiak_Survey",
+                    "Annual_Copepod_Community_Size_EGOA_Survey"    ,                  
+                     "Annual_Copepod_Community_Size_WGOA_Survey")]
+
+sblbiglong <- sblbigsub %>% pivot_longer(values_to = "value", names_to = "indicator", -YEAR)
+
+ggplot(sblbiglong, aes(YEAR, value)) + geom_point() + facet_wrap(~indicator, scales="free")
+
+ggplot(sblbiglong, aes(value)) + geom_histogram() + facet_wrap(~indicator, scales="free")
+
+#let's log "Annual_Heatwave_GOA_Model", "Summer_Sablefish_CPUE_Juvenile_GOA_Survey",                      
+#"Summer_Sablefish_CPUE_Juvenile_Nearshore_GOAAI_Survey",
+
+sblbigsub$log_Annual_Heatwave_GOA_Model <- log(sblbigsub$Annual_Heatwave_GOA_Model)
+sblbigsub$log_Summer_Sablefish_CPUE_Juvenile_GOA_Survey <- log(sblbigsub$Summer_Sablefish_CPUE_Juvenile_GOA_Survey)
+sblbigsub$log_Summer_Sablefish_CPUE_Juvenile_Nearshore_GOAAI_Survey <- log(sblbigsub$Summer_Sablefish_CPUE_Juvenile_Nearshore_GOAAI_Survey)
+
+sblbigsub <- sblbigsub[,c("YEAR",                                                     
+                           #"Annual_Heatwave_GOA_Model",                                
+                           "Annual_Sablefish_Growth_YOY_Middleton_Survey",             
+                           "Spring_Temperature_Surface_EGOA_Satellite",                
+                           "Spring_Temperature_Surface_GOA_Satellite",                 
+                           "Spring_Temperature_Surface_SEBS_Satellite",                
+                          # "Summer_Sablefish_CPUE_Juvenile_GOA_Survey",                
+                          # "Summer_Sablefish_CPUE_Juvenile_Nearshore_GOAAI_Survey",    
+                           "Summer_Temperature_250m_GOA_Survey",                       
+                           "Spring_Chlorophylla_Biomass_EGOA_Satellite",               
+                           "Spring_Chlorophylla_Biomass_GOA_Satellite",                
+                           "Spring_Chlorophylla_Biomass_SEBS_Satellite",               
+                           "Annual_Copepod_Community_Size_EGOA_Survey",                
+                           "Annual_Copepod_Community_Size_WGOA_Survey",                
+                           "log_Annual_Heatwave_GOA_Model",                            
+                           "log_Summer_Sablefish_CPUE_Juvenile_GOA_Survey",            
+                           "log_Summer_Sablefish_CPUE_Juvenile_Nearshore_GOAAI_Survey")]
+
+
+
+zsbl <- sblbigsub %>% #group_by(Year) %>%
+  mutate(Annual_Sablefish_Growth_YOY_Middleton_Survey_zscor=scale(Annual_Sablefish_Growth_YOY_Middleton_Survey),
+         Spring_Temperature_Surface_EGOA_Satellite_zscor=scale(Spring_Temperature_Surface_EGOA_Satellite),
+         Spring_Temperature_Surface_GOA_Satellite_zscor=scale(Spring_Temperature_Surface_GOA_Satellite),
+         Spring_Temperature_Surface_SEBS_Satellite_zscor=scale(Spring_Temperature_Surface_SEBS_Satellite),
+         Summer_Temperature_250m_GOA_Survey_zscor=scale(Summer_Temperature_250m_GOA_Survey),
+         Spring_Chlorophylla_Biomass_EGOA_Satellite_zscor=scale(Spring_Chlorophylla_Biomass_EGOA_Satellite),
+         Spring_Chlorophylla_Biomass_GOA_Satellite_zscor=scale(Spring_Chlorophylla_Biomass_GOA_Satellite),
+         Spring_Chlorophylla_Biomass_SEBS_Satellite_zscor=scale(Spring_Chlorophylla_Biomass_SEBS_Satellite),
+         Annual_Copepod_Community_Size_EGOA_Survey_zscor=scale(Annual_Copepod_Community_Size_EGOA_Survey),
+         Annual_Copepod_Community_Size_WGOA_Survey_zscor=scale(Annual_Copepod_Community_Size_WGOA_Survey),
+         log_Annual_Heatwave_GOA_Model_zscor=scale(log_Annual_Heatwave_GOA_Model),
+         log_Summer_Sablefish_CPUE_Juvenile_GOA_Survey_zscor=scale(log_Summer_Sablefish_CPUE_Juvenile_GOA_Survey),
+         log_Summer_Sablefish_CPUE_Juvenile_Nearshore_GOAAI_Survey_zscor=scale(log_Summer_Sablefish_CPUE_Juvenile_Nearshore_GOAAI_Survey))
+
+zsbllong <- zsbl %>% pivot_longer(values_to = "value", names_to = "indicator", -YEAR)
+
+
+ggplot(zsbllong, aes(value)) + geom_histogram() + facet_wrap(~indicator, scales="free")
+
+
+#join datasets together------
 
 
 
